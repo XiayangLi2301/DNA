8a297ecd71db886f6dad604ae376e9f6922a758d
XiayangLi2301 <lixiayang@tju.edu.cn>
2022-09-09 21:51:32 +0800

    update

--- correct/def_solve.py
+++
@@ -1,8 +0,0 @@
-from scipy import linalg
-import numpy as np
-
-def fun_solve(k,l,m):
-    A = np.array([[1,-1],[1,1]])
-    b = np.array([k-l,m])
-    x = linalg.solve(A,b)
-    return(x)
--- correct/right_dna.py
+++
@@ -1,8 +0,0 @@
-import count_S
-from count_S import count_S
-def right_code(right_dna):
-    k = len(right_dna)
-    S = count_S(right_dna)
-    return [k,S]
-
-
